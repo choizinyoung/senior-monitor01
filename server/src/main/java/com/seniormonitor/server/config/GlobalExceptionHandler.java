87385c76b6afc0a0657f4a2a9eaafb06f55c021c
choizinyoung <ligt69209@gmail.com>
2026-06-25 17:52:50 +0900

    fix: 에러 로그 추가

--- server/src/main/java/com/seniormonitor/server/config/GlobalExceptionHandler.java
+++ server/src/main/java/com/seniormonitor/server/config/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import com.seniormonitor.server.dto.ApiResponse;
 import com.seniormonitor.server.exception.BadRequestException;
 import com.seniormonitor.server.exception.ConflictException;
 import com.seniormonitor.server.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(BadRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -33,6 +37,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ApiResponse<?> handleUnexpected(Exception e) {
+        log.error("Unexpected error", e);
         return ApiResponse.fail("ERR_INTERNAL", "서버 오류가 발생했습니다.");
     }
 }
